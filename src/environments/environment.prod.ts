82722a2bea321aba71088f310e9b40d4beb68141
franroca18 <69211745+franroca18@users.noreply.github.com>
2022-07-03 11:42:44 +0200

    changed enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  serverPath: 'http://futbolservice-env.eba-vdnvjb8j.eu-west-1.elasticbeanstalk.com/',
+  apiBase: 'http://localhost:4200/app/',
+  env: 'prod'
 };
