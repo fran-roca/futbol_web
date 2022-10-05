0617b900fccaf87ad0f9b55c5bd47deee5fa7181
franroca18 <69211745+franroca18@users.noreply.github.com>
2022-10-05 19:39:43 +0200

    new page valoraciones and readme

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -11,4 +11,18 @@ export class Constants {
     public static TABLE_SOMATOTIPO:string = "somatotipo"
     public static TABLE_VALORACION:string = "valoracion"
     public static TABLE_VISUALIZACION:string = "visualizacion"
+
+    
+    public static ROLE_USER:string = "ROLE_USER"
+    public static ROLE_SCOUTER:string = "ROLE_SCOUTER"
+    public static ROLE_ADMIN:string = "ROLE_ADMIN"
+
+
+    public static URL_PARAM_ID:string = 'id'
+    public static URL_PARAM_ID_VALORACION:string = 'id_valoracion'
+    public static URL_PARAM_ID_JUGADOR:string = 'id_jugador'
+    public static URL_PARAM_DESCRIPCION:string = 'desc'
+    public static URL_PARAM_MIN:string = 'min'
+    public static URL_PARAM_NOMBRE:string = 'nombre'
+    public static URL_PARAM_ORDER_BY:string = 'order_by'
  }
